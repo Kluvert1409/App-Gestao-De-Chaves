0ba33f307861baaaf8a4ce0449fbc8ccf11ce627
Kluvert1409 <kluvertgabriel07@gmail.com>
2025-04-02 22:03:17 -0300

    Commit

--- app/src/main/java/com/github/Kluvert1409/devandroid/MainActivity.java
+++ app/src/main/java/com/github/Kluvert1409/devandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.github.Kluvert1409.devandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button botaoProfessor = findViewById(R.id.botao_professor);
+
+        botaoProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botaoPorteiro = findViewById(R.id.botao_porteiro);
+
+        botaoPorteiro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
