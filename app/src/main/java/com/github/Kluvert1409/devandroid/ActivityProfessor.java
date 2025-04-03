ffd3cfcfade262566fd51a7a418d5de6b9f13833
Kluvert1409 <kluvertgabriel07@gmail.com>
2025-04-02 22:50:12 -0300

    Commit

--- app/src/main/java/com/github/Kluvert1409/devandroid/ActivityProfessor.java
+++ app/src/main/java/com/github/Kluvert1409/devandroid/ActivityProfessor.java
@@ -1,9 +1,6 @@
 package com.github.Kluvert1409.devandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class ActivityProfessor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_professor);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button botaoLogin = findViewById(R.id.botao_login);
-
-        botaoLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ActivityProfessor.class);
-                startActivity(intent);
-            }
         });
     }
 }
