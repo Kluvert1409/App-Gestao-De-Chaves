c5e1f5f464c51b3f85ceb82af6824e9d78219224
Kluvert1409 <kluvertgabriel07@gmail.com>
2025-04-10 21:09:39 -0300

    Commit

--- app/src/main/java/com/github/Kluvert1409/devandroid/MainActivity.java
+++ app/src/main/java/com/github/Kluvert1409/devandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
