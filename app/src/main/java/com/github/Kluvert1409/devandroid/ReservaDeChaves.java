dfc307dc3ad148ebcd985abc9a4fc1abd5e03ea2
Kluvert <kluvertgabriel07@gmail.com>
2025-04-24 20:37:13 -0300

    Commit

--- app/src/main/java/com/github/Kluvert1409/devandroid/ReservaDeChaves.java
+++ app/src/main/java/com/github/Kluvert1409/devandroid/ReservaDeChaves.java
@@ -1,9 +1,6 @@
 package com.github.Kluvert1409.devandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActivityProfessor extends AppCompatActivity {
+public class ReservaDeChaves extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_professor);
+        setContentView(R.layout.activity_reserva_de_chaves);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-
-        Button botaoReservar = findViewById(R.id.botao_reserva);
-
-        botaoReservar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ActivityProfessor.this, ReservaDeChaves.class);
-                startActivity(intent);
-            }
         });
     }
 }
